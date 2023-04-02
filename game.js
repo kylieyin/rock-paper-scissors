b306f4f7c30196817910f4f3a6fb51307c9bc176
kylieyin <yin.kylie999@gmail.com>
2023-04-02 17:18:48 +1200

    changed input from prompt to butotn click

--- game.js
+++ game.js
@@ -1,20 +1,13 @@
-
+const buttons = document.querySelectorAll('button');
 const options = ["rock", "paper", "scissors"];
 // declares a constant variable defined as 'options' 
 // assigns an array to the variable which contains the values 'rock', 'paper' and 'scissors'
 // note: square brackets are used to define an array - which is a collection of values
+let playerScore = 0;
+let otterScore = 0;
 
-
-function getPlayerChoice() {
-    return prompt("Choose rock, paper, or scissors:").toLowerCase();
-}
-// declares a constant variable defined as 'playerChoice'
-// the function 'prompt()' displays a dialog box that asks player to input text and returns text entered 
-// the function '.toLowerCase() converts the player's input to lowercase so it match the options in the array
-
-function getOtterChoice() {
-    const otterChoice = options[Math.floor(Math.random() * options.length)];
-    return otterChoice;
+function otterPlay() {
+    return options[Math.floor(Math.random() * options.length)];
 }
 
 // declares a constant variable defined as 'computerChoice'
@@ -28,36 +21,56 @@ function getOtterChoice() {
 // this gives a random index to select a random option from the array
 // note: the square brackets are used to access an element in the 'options' array 
 
-function determineWinner(playerChoice, otterChoice) {
-    if (playerChoice === otterChoice) {
-        return "it's a tie!";
-    }
-    const outcomes = {
-        rock: {rock: "tie", paper: "lose", scissors: "win"},
-        paper: {rock: "win", paper: "tie", scissors: "lose"},
-        scissors: {rock: "lose", paper: "win", scissors: "tie"}
-    };
-    const outcome = outcomes[playerChoice][otterChoice];
-    const message = {
-        win: "you win!",
-        lose: "you lose!",
-        tie: "its a tie!"
-    };
-    return message[outcome];
+function disableButtons() {
+    buttons.forEach(button => {
+        button.disabled = true
+    });
 }
 
-function playGame() {
-  let playAgain = true;
-  while (playAgain) {
-    const playerChoice = getPlayerChoice();
-    const otterChoice = getOtterChoice();
-    console.log(`you chose ${playerChoice}.`);
-    console.log(`otter chose ${otterChoice}.`); 
-    console.log(determineWinner(playerChoice, otterChoice));
-    const choice = prompt("play again? (type yes/no)");
-    if (choice.toLowerCase() !== "yes") {
-        playAgain = false;
+function roundStart(playerSelection) {
+    let otterSelection = otterPlay();
+    let result = "";
+
+    if (playerSelection === "rock" && otterSelection === "scissors" ||
+        playerSelection === "paper" && otterSelection === "rock" ||
+        playerSelection === "scissors" && otterSelection === "paper") 
+    {
+        playerScore += 1;
+        result = "you win this round! " + playerSelection + " beats " + otterSelection + 
+        "<br><br>Player Score: " + playerScore + "<br>Otter Score: " + otterScore;
+        
+        if (playerScore === 5) {
+            result += "<br><br>you won the game! reload page to play again";
+            disableButtons();
+        }
     }
-  }
+    
+    else if (playerSelection === otterSelection) {
+        result = "it's a tie! you both chose " + playerSelection + "<br><br>Player Score: " + playerScore + "<br>Otter Score: " + otterScore;
+               
+    } 
+    
+    else {
+        otterScore += 1;
+        result = "you lost this round! " + otterSelection + " beats " + playerSelection + 
+        "<br><br>Player Score: " + playerScore + "<br>Otter Score: " + otterScore; 
+        if (otterScore === 5) {
+        result += "<br><br>otter won the game! reload page to play again"
+        disableButtons();
+      }
+    }
+
+    return result;
+
 }
+// declares a constant variable defined as 'playerChoice'
+// the function 'prompt()' displays a dialog box that asks player to input text and returns text entered 
+// the function '.toLowerCase() converts the player's input to lowercase so it match the options in the array
+
+
+buttons.forEach(button => {
+    button.addEventListener("click", function(){
+        document.getElementById("result").innerHTML = roundStart(button.id);
+    });
+});
 
